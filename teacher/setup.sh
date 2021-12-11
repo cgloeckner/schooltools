efc24b35aa113a5330c5436d224946ae0631496e
cgloeckner <cgloeckner@freenet.de>
2021-12-11 09:49:33 +0100

    added netbios name; removed unused mkdir

--- teacher/setup.sh
+++ teacher/setup.sh
@@ -12,14 +12,14 @@ sudo apt-get install samba
 # setup directories
 mkdir ${TEACHER}
 mkdir ${STUDENT}
-mkdir ${SOFTWARE}
 chmod o=rwx ${STUDENT}
 
 # prepare samba share config
+HOSTNAME="[global]\nnetbios name = $1"
 AUSGABE="[Ausgabe]\npath = ${TEACHER}\nread only = yes\nguest ok = yes"
 EINSAMMELN="[Einsammeln]\npath = ${STUDENT}\nwritable = yes\nguest ok=yes"
 INSTALLER="[Software]\npath = /var/cache/apt/archives\nread only = yes\nguest ok = yes" 
-echo -e "${AUSGABE}\n\n${EINSAMMELN}\n\n${INSTALLER}" | sudo tee -a ${SMB_SHARES} > /dev/null
+echo -e "${HOSTNAME}\n\n${AUSGABE}\n\n${EINSAMMELN}\n\n${INSTALLER}" | sudo tee -a ${SMB_SHARES} > /dev/null
 
 # add shares to samba and restart it
 echo "include = ${SMB_SHARES}" | sudo tee -a ${SMB_CONF} > /dev/null
